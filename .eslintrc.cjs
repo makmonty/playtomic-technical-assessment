ce28a5fa72594eabd898d510adde966dc37283ca
Ángel Brasero Moreno <makmonty@users.noreply.github.com>
2025-10-08 10:54:11 +0200

    Added "semi" and "quotes" eslint rules to keep a consistent style.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'semi': ['error', 'never'],
   },
   settings: {
     react: {
